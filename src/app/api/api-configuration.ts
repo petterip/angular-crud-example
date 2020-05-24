bc9c91f611b39210a818d48d12eb2eb09b722012
Petteri Ponsimaa <petterip@gmail.com>
2020-05-24 14:40:40 +0300

    changed config to Heroku

--- src/app/api/api-configuration.ts
+++ src/app/api/api-configuration.ts
@@ -8,8 +8,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = 'http://localhost:8080';
-  // rootUrl: string = 'https://patient-repository-api.herokuapp.com';
+  rootUrl: string = 'https://patient-repository-api.herokuapp.com';
 }
 
 /**
